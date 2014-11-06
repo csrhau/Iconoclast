7770a2daea93efcdacd840377a43439b86e6b6c7
Stephen Roberts <stephenianroberts@gmail.com>
2014-11-06 15:54:41 +0000

    this be fucked

---
+++ Benchmarks/threaded_loops/src/benchmark.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+#include "benchmark.h"
+
+static void * nop_benchmark(void * args) {
+//  asm volatile 
+  while(1) {}
+}
+
+void run_benchmark(int threads, enum BENCHMARK benchmark) {
+  int thread;
+  pthread_t thread_handles[threads];
+  void * (*benchmark_function)(void *);
+  void ** retval = NULL;
+
+  switch (benchmark) {
+    case NOP:
+      benchmark_function = &nop_benchmark; 
+      break;
+    default:
+      fprintf(stderr, "Invalid benchmark! exiting");
+      exit(EXIT_FAILURE);
+      break;
+  }
+
+  for (thread = 0; thread < threads; ++thread) {
+    pthread_create(&thread_handles[thread], NULL,
+                   benchmark_function,
+                   (void *) NULL);
+  }
+  for (thread = 0; thread < threads; ++thread) {
+    pthread_join(thread_handles[thread], retval);
+  }
+}
