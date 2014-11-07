262e38ab3153ae7be774609f9c1ab8b2e71a357d
Stephen Roberts <stephenianroberts@gmail.com>
2014-11-07 09:05:18 +0000

    Threaded runner

---
+++ Benchmarks/threaded_loops/experiment/runner.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+modprobe msr
+BASEDIR=../../..
+KNOWNS="PAPI_TOT_INS,PAPI_L1_LDM,PAPI_L1_STM,PAPI_TLB_DM,PAPI_L2_TCM,PAPI_STL_ICY"
+DELAY=360
+
+for FREQ in `$BASEDIR/Scripts/freq_avail.sh`; do
+  echo Experiment: $FREQ
+  for THREADCOUNT in 1 2 3 4; do
+    cpufreq-set -g performance --min $FREQ --max $FREQ -r
+    cpufreq-info | grep 'current policy'
+    LD_PRELOAD=/home/sir/Dropbox/Work/Code/wattson/sleuth/bin/sleuth.so SLEUTH_OUTPUT_FILE=$(pwd)/results/freq_${FREQ}_threads_${THREADCOUNT}_ssr_64ONE.out SLEUTH_ADDITIONAL_METRICS=${KNOWNS} SLEUTH_SAMPLE_RATE=64000000 ../build/lbench -t $THREADCOUNT &
+    sleep $DELAY
+    kill $!
+  done
+done
